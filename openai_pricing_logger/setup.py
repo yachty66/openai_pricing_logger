b75821381809c4d1eae454194a7626cdef782211
Max Hager <maxhager28@gmail.com>
2023-04-12 22:27:15 -0700

    ss,

--- openai_pricing_logger/setup.py
+++ openai_pricing_logger/setup.py
@@ -6,6 +6,7 @@ setup(
     author="Max Hager",
     author_email="maxhager28@gmail.com",
     description="A package to log OpenAI API costs",
+    url="https://github.com/yachty66/openai_pricing_logger",
     packages=find_packages(),
     package_data={"logger_package": ["pricing.json"]},
     include_package_data=True,
